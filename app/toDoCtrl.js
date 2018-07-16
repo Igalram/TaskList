46fed8ccee257b0d285b965650f90ce4b3ca0867
Igalram <ram8555@gmail.com>
2018-07-16 15:51:55 +0300

    starting dynamic display of items

--- app/toDoCtrl.js
+++ app/toDoCtrl.js
@@ -1,5 +1,11 @@
 app.controller('toDoCtrl', function ($scope, $http, $location, toDoSrv) {
     
+  $scope.tasks = toDoSrv.getTaskList();
+
+  $scope.setTask = function () {
+    toDoSrv.setTask($scope.newTask);
+  }
+
   
     
 
