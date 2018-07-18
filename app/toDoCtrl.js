6b1c14957a92ced0f9775b57d279e309e34fcbd5
iiram <ram8555@gmail.com>
2018-07-18 14:01:55 +0300

    activiting checking and deleting

--- app/toDoCtrl.js
+++ app/toDoCtrl.js
@@ -2,6 +2,8 @@ app.controller('toDoCtrl', function ($scope, $http, $location, toDoSrv) {
     
   $scope.tasks = toDoSrv.getTaskList();
 
+  $scope.mouseOver = false;
+
   $scope.setTask = function () {
     toDoSrv.setTask($scope.newTask);
   }
